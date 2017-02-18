08f6b7788cb511f3f1c89a88d6c02b7de3d09353
Arjun Mayilvaganan <arjunmayilvaganan@gmail.com>
2017-02-19 00:27:45 +0530

    add error logging, missing semicolons. Rewrite certain log statements

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var mongojs = require('mongojs');
 var path = require('path');
 var sa = require('superagent');
 var rlogger = require('morgan');
-var fs = require('fs')
+var elogger = require('winston');
+var fs = require('fs');
 
 var app = express();
 var port = process.env.PORT || 8080;
@@ -18,8 +19,9 @@ var miscrecords = db.collection('miscrecords');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('static'));
-app.use(rlogger('common', {stream: fs.createWriteStream('./access.log', {flags: 'a'})}))
-app.use(rlogger('dev'))
+app.use(rlogger('common', {stream: fs.createWriteStream('./access.log', {flags: 'a'})}));
+app.use(rlogger('dev'));
+elogger.add(elogger.transports.File, {filename: './error.log', level: 'error', handleExceptions: true});
 
 app.get('/', function(req, res) {
 	res.send(index.html);
@@ -27,15 +29,15 @@ app.get('/', function(req, res) {
 
 app.get('/checkregistered', function(req, res) {
 	var number = req.query.number;
-	console.log('Entered number:',number);
+	console.log('Check if already registered, Mobile:', number);
 	registrations.findOne({"mobile": number}, function(err, doc) {
 		if(err)
-			console.log(err);
+			elogger.log('error', err);
 		else
 		{
 			if(doc)
 			{
-				console.log(doc);
+				console.log('Record pertaining to the requested number:\n', doc);
 				res.send('registered');
 			}
 			else
@@ -43,11 +45,13 @@ app.get('/checkregistered', function(req, res) {
 				var appKey = process.env.otpAppKey;
 				var reqBody = {'countryCode': '91', 'mobileNumber': number, 'getGeneratedOTP': true};
 				sa.post('https://sendotp.msg91.com/api/generateOTP').set('application-Key', appKey).send(reqBody).end(function(err, res) {
-					if(err) console.log(err);
+					if(err)
+						elogger.log('error', err);
 					else
 					{
 						miscrecords.findOne({"otpApiCalls": {$exists: true}}, function(err, doc) {
-							if(err) console.log(err);
+							if(err)
+								elogger.log('error', err);
 							else
 							{
 								if(doc)
@@ -60,7 +64,7 @@ app.get('/checkregistered', function(req, res) {
 							}
 						})
 						otps.update({"mobile": number}, {"mobile": number, "otp": res.body.response.oneTimePassword}, {"upsert": true});
-						console.log(res.body.response.oneTimePassword);
+						console.log('OTP response from sendOTP endpoint:', res.body.response.oneTimePassword);
 					}
 				});
 				res.send('not registered');
@@ -72,14 +76,14 @@ app.get('/checkregistered', function(req, res) {
 app.get('/verifyotp', function(req, res) {
 	var number = req.query.number;
 	var otp = req.query.otp;
-	console.log('Verify OTP values:', number, otp);
+	console.log('OTP Verification -', 'Number:', number, 'OTP:', otp);
 	otps.findOne({"mobile": number}, function(err, doc) {
-		if(err) console.log(err);
+		if(err)
+			elogger.log('error', err);
 		else
 		{
 			if(doc)
 			{
-				console.log(doc);
 				if(doc.otp == otp)
 					res.send('true');
 				else
@@ -90,14 +94,23 @@ app.get('/verifyotp', function(req, res) {
 })
 
 app.post('/register', function(req, res) {
+	if(!req.body.mobile || req.body.mobile == '')
+		res.send('One or more values missing');
 	console.log(req.body);
 	registrations.insert(req.body, function(err) {
-		if(err) console.log(err);
-		else console.log("Student data loaded successfully!");
+		if(err)
+		{
+			elogger.log(err);
+			res.send('Error occurred during registration');
+		}
+		else
+		{
+			console.log("New Student registration Successful");
+			res.send("New Student registration Successful");
+		}
 	});
-	res.send('thanks for registration.');
 })
 
 
 app.listen(port);
-console.log('Server started! At http://localhost:' + port);
+console.log('Server started!\n Listening now at http://localhost:' + port);
